15831291d17fc65078c9185f3fb33f165588bc7e
rashed62 <mdrashedbiswas83@gmail.com>
2024-05-11 22:11:49 +0600

    completed the delete method

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ async function run() {
     app.delete('/services/:id', async(req, res) => {
         const id = req.params.id;
         const query =  {_id: new ObjectId(id)}
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result);
     })
 
     // Send a ping to confirm a successful connection
